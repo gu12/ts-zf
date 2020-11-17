2deadcad6a4e22f91b14a2a49b8d762199091145
zcxiao <zcxiao@zcxiaodeMacBook-Pro.local>
2020-11-17 20:55:54 +0800

    异构枚举不能反推

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,9 +1,22 @@
 (function () {
-	'use strict';
+   'use strict';
 
-	var a = '123';
-	console.log(a);
-	debugger;
+   var a = '123';
+   console.log(a, 1);
+   var tuple = ['2', 2];
+   // tuple[3] =1
+   //能push
+   tuple.push(1);
+   console.log(tuple);
+   // let arr1:number[] = [1,'2']
+   var USER;
+   (function (USER) {
+       // 'A' = 'A1', //这样写就不能反推了
+       USER[USER["A"] = 0] = "A";
+   })(USER || (USER = {}));
+   //const enum xx 就是常量枚举 不能用下标查询
+   console.log(0 /* A */);
+   console.log(0 /* A */);
 
 }());
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 
 let a:string = '123'
-console.log(a)
-debugger
+console.log(a,1)
 
+let tuple:[string,number] = ['2',2]
+// tuple[3] =1
+//能push
+tuple.push(1)
+
+console.log(tuple)
+
+// let arr1:number[] = [1,'2']
+
+enum USER {
+    'A' = 'A1', //这样写就不能反推了
+    
+
+}
+console.log(USER['A1'])
+
+const enum USE1R {
+    // 'A' = 'A1', //
+     A
+ 
+ }
+ //const enum xx 就是常量枚举 不能用下标查询
+console.log(USE1R.A)
+console.log(USE1R.A)
+
+//void空类型 严格模式下只能将unfefined赋值给void 非严格可以把null赋值
+let aa:void = undefined
 
